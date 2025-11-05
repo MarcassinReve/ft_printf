aa2b2b6a8afb2516236b2d64e52edd055de015a1
Florian Blanc <fblanc@student.42lausanne.ch>
2025-11-05 16:43:23 +0100

    olalalah nom d un marcassin les variatic euuhhhh

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fblanc <fblanc@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 14:50:37 by fblanc            #+#    #+#             */
-/*   Updated: 2025/10/30 15:43:54 by fblanc           ###   ####lausanne.ch   */
+/*   Updated: 2025/11/05 16:42:49 by fblanc           ###   ####lausanne.ch   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 
 int	ft_printf(const char *format, ...)
 {
-	va_list list;
+	int		total;
+	va_list	list;
 
-	va_start(list, format)
+	total = 0;
+	va_start(list, format);
+
+	if (format == "%" && *(format +1)) // trouver le %qqchose et lenvoyer au dispatcher
+	{
+		format++;
+		dispatcher(*format, &list);
+	}
+	va_end(list);
+	return (total);
 }
 
 //va_arg(list, type) => acces the next argument. Cant be call more than the number of arg pass. type is the type of arg to retrieve
